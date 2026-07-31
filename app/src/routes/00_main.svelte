b9552ac6637da7ba4476bed881d01c1ea56f7371
Lightshadow244 <richi.lightshadow@gmail.com>
2026-07-31 10:39:35 +0200

    update

---
+++ app/src/routes/00_main.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { resolve } from '$app/paths';
+    import Selection from "./01_selection.svelte";
+    let page: number = $state(0);
+</script>
+
+<nav class="navbar">
+    <a class="home-link" href={resolve("/")}>Open Tournament Software</a>
+</nav>
+
+{#if page == 0}
+    <Selection />
+{/if}
+
+<style>
+    .navbar{
+        display: flex;
+    }
+    .home-link{
+        
+    }
+</style>
